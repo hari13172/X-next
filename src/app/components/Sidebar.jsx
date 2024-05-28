c3b918c5f1222c065aeaec3ead6ebdef2c4ac867
hari <haridiablo72@gmail.com>
2024-05-28 16:12:15 +0530

    learning

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -1,40 +1,57 @@
 "use client";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
-
 import React from "react";
 import { FaXTwitter } from "react-icons/fa6";
-import { HiHome } from "react-icons/hi";
+import { HiDotsHorizontal, HiHome } from "react-icons/hi";
 
 export default function Sidebar() {
   const { data: session } = useSession();
+  console.log(session);
   return (
-    <div className="flex flex-col gap-4 p-4 items-center">
-      <Link href="/">
-        <FaXTwitter className="w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200" />
-      </Link>
-      <Link
-        href="/"
-        className="flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit"
-      >
-        <HiHome className="w-7 h-7" />
-        <span className="font-bold hidden xl:inline">Home</span>
-      </Link>
+    <div className="flex flex-col p-3 justify-between h-screen">
+      <div className="flex flex-col gap-4 p-4 items-center">
+        <Link href="/">
+          <FaXTwitter className="w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200" />
+        </Link>
+        <Link
+          href="/"
+          className="flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit"
+        >
+          <HiHome className="w-7 h-7" />
+          <span className="font-bold hidden xl:inline">Home</span>
+        </Link>
 
-      {session ? (
-        <button
-          onClick={() => signIn()}
-          className="bg-blue-500 text-white w-48 h-9 rounded-full  hover:brightness-95 shadow-md transition-all duration-200 hidden xl:inline font-semibold"
-        >
-          Sign in
-        </button>
-      ) : (
-        <button
-          onClick={() => signOut()}
-          className="bg-blue-500 text-white w-48 h-9 rounded-full  hover:brightness-95 shadow-md transition-all duration-200 hidden xl:inline font-semibold"
-        >
-          Sign Out
-        </button>
+        {session ? (
+          <button
+            onClick={() => signIn()}
+            className="bg-blue-500 text-white w-48 h-9 rounded-full  hover:brightness-95 shadow-md transition-all duration-200 hidden xl:inline font-semibold"
+          >
+            Sign in
+          </button>
+        ) : (
+          <button
+            onClick={() => signOut()}
+            className="bg-blue-500 text-white w-48 h-9 rounded-full  hover:brightness-95 shadow-md transition-all duration-200 hidden xl:inline font-semibold"
+          >
+            Sign Out
+          </button>
+        )}
+      </div>
+
+      {session && (
+        <div className="text-gray-700 text-sm flex items-center cursor-pointer hover:bg-gray-200 rounded-full transition-all duration-200">
+          <img
+            src={session.user.image}
+            alt="img"
+            className="h-10 w-10 rounded-full xl:mr-2"
+          />
+          <div className="hidden xl:inline">
+            <h1 className="font-bold">{session.user.name}</h1>
+            <p className="text-gray-500">{session.user.username}</p>
+          </div>
+          <HiDotsHorizontal className="h-5 xl:ml-8 hidden xl:inline" />
+        </div>
       )}
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import Input from './components/Input';
 
 export default function page() {
   return (
-    <div className='text-blue-600'>hello world</div>
+    <div className='max-w-xl mx-auto'>
+      <div className='py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200'>
+        <h2 className='text-lg sm:text-xl font-bold'>Home</h2>
+      </div>
+      <Input />
+    </div>
   )
 }
